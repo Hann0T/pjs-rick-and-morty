f6e1824f26a86af8a7530eb365ff030917b3f935
Hans <hansolivas1@gmail.com>
2021-08-11 11:17:15 -0500

    first Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logo from "../images/logo.png";
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt="rick and morty logo" />
+        </header>
+    );
+}
+
+export default Header;
